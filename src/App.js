5e394ea92fb5833c89e04fb1127afe1bee2bb375
Kathleen Garitty <katgaritty@gmail.com>
2017-06-01 20:23:06 -0500

    updating App files

--- src/App.js
+++ src/App.js
@@ -1,106 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import GameBoard from './components/GameBoard';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-{/*
-          <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <div className="App-header">
+          <h1>1 to 100</h1>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-*/}
-        <ButtonField/>
+        <GameBoard />
       </div>
     );
   }
-}
-
-class ButtonField extends Component {
-    constructor(){
-        super();
-        this.state = {
-            curNumber: 1,
-            buttonField: [],
-        };
-        for (var i = 1; i <= 100; i++){
-            this.state.buttonField.push(this.renderButton(i));
-        }
-        this.setState({buttonField: this.shuffleArray(this.state.buttonField)});
-    }
-    
-    render() {
-        return (
-        <div>
-            <div className="App-header"> 
-                <div className="App-intro"> 
-                    Current Number: {this.state.curNumber}
-                </div> 
-            </div>
-            <div>{this.state.buttonField}</div>
-        </div>
-        );
-    }
-
-    renderButton(i) {
-        return (
-            <NumberButton 
-                value={i} 
-                onClick={() => this.handleClick(i)}
-            />
-        );
-    }
-    
-    handleClick(i) {
-        if (i == this.state.curNumber){
-            this.setState({curNumber: this.state.curNumber + 1});
-        }
-        this.render()
-    }
-
-    shuffleArray(array) {
-        var currentIndex = array.length, temporaryValue, randomIndex;
-
-          // While there remain elements to shuffle...
-          while (0 !== currentIndex) {
-
-            // Pick a remaining element...
-            randomIndex = Math.floor(Math.random() * currentIndex);
-            currentIndex -= 1;
-
-            // And swap it with the current element.
-            temporaryValue = array[currentIndex];
-            array[currentIndex] = array[randomIndex];
-            array[randomIndex] = temporaryValue;
-          }
-
-          return array;
-}   
-
-}
-
-class NumberButton extends Component {
-    constructor() {
-        super();
-        this.state = {
-            value: null,
-        };
-    }
-    
-    render() {
-        this.state.value = this.props.value
-        return (
-            <button type="button" className="NumberButton" onClick={() => this.props.onClick()}>
-                {this.props.value}
-            </button>
-
-        );
-    }
 }
 
 export default App;
